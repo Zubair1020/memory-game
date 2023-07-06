7b2bcc2595eb3bab7516f731ddad49b244d585d6
Zubair1020~ <rakibhossianraju.com>
2023-07-07 01:27:59 +0600

    magic match react

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import Card from "./components/card/card";
+
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  const [{ choiceOne, choiceTow }, setChoice] = useState({
+    choiceOne: null,
+    choiceTow: null,
+  });
+
+  useEffect(() => {
+    if (choiceOne && choiceTow) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTow.src) {
+        setCards((preVal) =>
+          preVal.map((card) =>
+            card.src === choiceOne.src ? { ...card, matched: true } : card
+          )
+        );
+        resetTurns();
+      } else {
+        setTimeout(() => {
+          resetTurns();
+        }, 1000);
+      }
+    }
+  }, [choiceOne, choiceTow]);
+
+  useEffect(() => {
+    shuffledCards();
+  }, []);
+
+  // shuffle cards
+  const shuffledCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((cardImage) => ({ ...cardImage, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+    setChoice({
+      choiceOne: null,
+      choiceTow: null,
+    });
+  };
+
+  // handel a choice
+  const handelChoice = (card) => {
+    !choiceOne
+      ? setChoice((preVal) => ({ ...preVal, choiceOne: card }))
+      : setChoice((preVal) => ({ ...preVal, choiceTow: card }));
+  };
+
+  const resetTurns = () => {
+    setChoice({
+      choiceOne: null,
+      choiceTow: null,
+    });
+    setTurns(turns + 1);
+    setDisabled(false);
+  };
+
+  return (
+    <div className={styles.app}>
+      <h1>Magic Match</h1>
+      <button onClick={shuffledCards}>New Game</button>
+
+      <div className={styles.card_gird}>
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            handelChoice={handelChoice}
+            flipped={card === choiceOne || card === choiceTow || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+
+      <p>Turns: {turns}</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/card/card.jsx
@@ -0,0 +1,21 @@
+import styles from "./card.module.css";
+
+const Card = ({ card, handelChoice, flipped, disabled }) => {
+  return (
+    <div className={styles.card}>
+      <div className={flipped && styles.flipped}>
+        <img
+          className={styles.front}
+          src={card.src}
+        />
+        <img
+          className={styles.back}
+          src="/img/cover.png"
+          onClick={() => !disabled && handelChoice(card)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
